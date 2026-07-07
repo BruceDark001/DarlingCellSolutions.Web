f552358f4f8a26902adf82c36ef1193d582fab6c
BruceDark001 <brucedota06@gmail.com>
2026-07-07 03:18:36 -0500

    Agregar archivos de proyecto.

---
+++ DarlingCellSolutions.Domain/Entidades/Base/EntidadBase.cs
@@ -0,0 +1,14 @@
+﻿namespace DarlingCellSolutions.Domain.Entidades.Base;
+
+public abstract class EntidadBase
+{
+    public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+    public DateTime? FechaModificacion { get; set; }
+
+    public string? UsuarioCreacion { get; set; }
+
+    public string? UsuarioModificacion { get; set; }
+
+    public bool Estado { get; set; } = true;
+}
---
+++ DarlingCellSolutions.Domain/Entidades/Seguridad/Rol.cs
@@ -0,0 +1,17 @@
+﻿using DarlingCellSolutions.Domain.Entidades.Base;
+
+namespace DarlingCellSolutions.Domain.Entidades.Seguridad;
+
+public class Rol : EntidadBase
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public string? Descripcion { get; set; }
+
+    // Propiedades de navegación
+    public ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+
+    public ICollection<RolPermiso> RolesPermisos { get; set; } = new List<RolPermiso>();
+}
